9dcbebf0a83838ea3d214c839c6e6b05e3d9a374
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2021-01-19 21:12:15 +0500

    Update Zoom_Api.php

--- Zoom_Api.php
+++ Zoom_Api.php
@@ -34,47 +34,46 @@ class Zoom_Api
 	}	
 	
 	//function to create meeting
-    public function createMeeting($data = array())
-    {
-        $post_time  = $data['start_date'];
-        $start_time = gmdate("Y-m-d\TH:i:s", strtotime($post_time));
-		
-        $createMeetingArray = array();
-        if (!empty($data['alternative_host_ids'])) {
-            if (count($data['alternative_host_ids']) > 1) {
-                $alternative_host_ids = implode(",", $data['alternative_host_ids']);
-            } else {
-                $alternative_host_ids = $data['alternative_host_ids'][0];
-            }
-        }
-		
-		
-		$createMeetingArray['topic']      = $data['topic'];
-        $createMeetingArray['agenda']     = !empty($data['agenda']) ? $data['agenda'] : "";
-        $createMeetingArray['type']       = !empty($data['type']) ? $data['type'] : 2; //Scheduled
-        $createMeetingArray['start_time'] = $start_time;
-        $createMeetingArray['timezone']   = 'Asia/Tashkent';
-        $createMeetingArray['password']   = !empty($data['password']) ? $data['password'] : "";
-        $createMeetingArray['duration']   = !empty($data['duration']) ? $data['duration'] : 60;
-		
-		$createMeetingArray['settings']   = array(
-            'join_before_host'  => !empty($data['join_before_host']) ? true : false,
-            'host_video'        => !empty($data['option_host_video']) ? true : false,
-            'participant_video' => !empty($data['option_participants_video']) ? true : false,
-            'mute_upon_entry'   => !empty($data['option_mute_participants']) ? true : false,
-            'enforce_login'     => !empty($data['option_enforce_login']) ? true : false,
-            'auto_recording'    => !empty($data['option_auto_recording']) ? $data['option_auto_recording'] : "none",
-            'alternative_hosts' => isset($alternative_host_ids) ? $alternative_host_ids : ""
-        );
+    	public function createMeeting($data = array())
+    	{
+		$post_time  = $data['start_date'];
+		$start_time = gmdate("Y-m-d\TH:i:s", strtotime($post_time));
 
+		$createMeetingArray = array();
+		if (!empty($data['alternative_host_ids'])) {
+		    if (count($data['alternative_host_ids']) > 1) {
+			$alternative_host_ids = implode(",", $data['alternative_host_ids']);
+		    } else {
+			$alternative_host_ids = $data['alternative_host_ids'][0];
+		    }
+		}
+
+
+		$createMeetingArray['topic']      = $data['topic'];
+		$createMeetingArray['agenda']     = !empty($data['agenda']) ? $data['agenda'] : "";
+		$createMeetingArray['type']       = !empty($data['type']) ? $data['type'] : 2; //Scheduled
+		$createMeetingArray['start_time'] = $start_time;
+		$createMeetingArray['timezone']   = 'Asia/Tashkent';
+		$createMeetingArray['password']   = !empty($data['password']) ? $data['password'] : "";
+		$createMeetingArray['duration']   = !empty($data['duration']) ? $data['duration'] : 60;
+
+		$createMeetingArray['settings']   = array(
+            		'join_before_host'  => !empty($data['join_before_host']) ? true : false,
+            		'host_video'        => !empty($data['option_host_video']) ? true : false,
+            		'participant_video' => !empty($data['option_participants_video']) ? true : false,
+            		'mute_upon_entry'   => !empty($data['option_mute_participants']) ? true : false,
+            		'enforce_login'     => !empty($data['option_enforce_login']) ? true : false,
+            		'auto_recording'    => !empty($data['option_auto_recording']) ? $data['option_auto_recording'] : "none",
+            		'alternative_hosts' => isset($alternative_host_ids) ? $alternative_host_ids : ""
+        	);
 
 		return $this->sendRequest($createMeetingArray);
-
-    }	
+	}	
 	
 	//function to send request
-    protected function sendRequest($data)
-    {
+    	protected function sendRequest($data)
+    	{
+		//Enter_Your_Email
 		$request_url = "https://api.zoom.us/v2/users/Enter_Your_Email/meetings";
 		
 		$headers = array(
@@ -85,26 +84,26 @@ class Zoom_Api
 		
 		$postFields = json_encode($data);
 		
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL => $request_url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $postFields,
-            CURLOPT_HTTPHEADER => $headers,
-        ));
+        	$ch = curl_init();
+        	curl_setopt_array($ch, array(
+            	CURLOPT_URL => $request_url,
+	    	CURLOPT_RETURNTRANSFER => true,
+	    	CURLOPT_ENCODING => "",
+	    	CURLOPT_MAXREDIRS => 10,
+	    	CURLOPT_TIMEOUT => 30,
+	    	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	    	CURLOPT_CUSTOMREQUEST => "POST",
+	    	CURLOPT_POSTFIELDS => $postFields,
+	    	CURLOPT_HTTPHEADER => $headers,
+        	));
 
-        $response = curl_exec($ch);
-        $err = curl_error($ch);
-        curl_close($ch);
-        if (!$response) {
-            return $err;
-        }
-        return json_decode($response);
-
-    }
+        	$response = curl_exec($ch);
+        	$err = curl_error($ch);
+        	curl_close($ch);
+        	if (!$response) {
+            		return $err;
+		}
+        	return json_decode($response);
+	}
 }
+
